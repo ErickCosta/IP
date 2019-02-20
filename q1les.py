f36d3f1e404761d8c8052ece05614cc77db5c100
Erick Costa <erick.costa.ti@gmail.com>
2019-02-19 21:37:35 -0300

    Questão 1 da lista de entrada e saída

---
+++ q1les.py
@@ -0,0 +1,13 @@
+valorCombustivel = 0
+valorAbastecer = 0
+
+print("Informe o valor do combustível: ")
+valorCombustivel = input()
+
+print("Informe o valor a ser abastecido: ")
+valorAbastecer = input()
+
+resultado = float(valorAbastecer) / float(valorCombustivel)
+
+print("A quantidade é:", resultado, "litros")
+

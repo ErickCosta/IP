14c9fd29a7cd6efe57d03c9b01e3db10ffa7fd5a
Erick Costa <erick.costa.ti@gmail.com>
2019-02-26 08:45:47 -0300

    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+x1 = 0
+x2 = 0
+x1 = input("Informe o primeiro valor: ")
+x2 = input("Informe o segundo valor: ")
+x1 = float(x1)
+x2 = float(x2)
+print("A soma dos dois valores é:", x1+x2)
+print("A subtração dos dois valores é:", x1-x2)
+print("A multiplicação dos dois valores é:", x1*x2)
+print("A divisão dos dois valores é:", x1/x2)
---
+++ teste.py
@@ -0,0 +1,10 @@
+print("Erick John Fidelis Costa")
+print(10+10)
+primeira = 4
+segunda = 5
+terceira = 10
+print("Essa é a primeira variável:", primeira)
+print("Essa é a segunda variável:", segunda)
+print("Essa é a terceira variável:", terceira)
+idade = 0
+idade = input("Informe sua idade: ")
